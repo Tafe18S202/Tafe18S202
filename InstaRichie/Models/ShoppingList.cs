9b54349142a7cf51339440403fd26b75d26d59a1
Luke Mills <lsm.web.contact@gmail.com>
2018-09-03 14:49:49 +0930

    Creation of shopping list branch features. Class, xaml and shell info.
    Signed-off-by: Luke Mills <lsm.web.contact@gmail.com>

---
+++ InstaRichie/Models/ShoppingList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+namespace StartFinance.Models
+{
+    class ShoppingList
+    {
+        [PrimaryKey][AutoIncrement]
+        public int ShoppingItemID { get; set; }
+        [NotNull]
+        public string ShopName { get; set; }
+        [NotNull]
+        public string NameOfItem { get; set; }
+        [NotNull]
+        public string ShoppingDate { get; set; } // TO DO, implement dateTime.
+        [NotNull]
+        public double PriceQuoted { get; set; }
+    }
+}
