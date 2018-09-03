e8f4950ad6678066aaac388c621f9f2f22a42d33
000316926 <chris.paturzo-elliott@student.tafesa.edu.au>
2018-09-03 12:23:55 +0930

    Initial commit add appointments.cs

---
+++ InstaRichie/Models/Appointments.cs
@@ -0,0 +1,27 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    class Appointments
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [NotNull]
+        public string EventName { get; set; }
+
+        [NotNull]
+        public string Location { get; set; }
+
+        [NotNull]
+        public DateTime EventDate { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+}
